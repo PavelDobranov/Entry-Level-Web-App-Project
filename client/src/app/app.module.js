9a98cd1c8b3f65fbbb27505df4bc9e9ad3e1c79a
Pavel Dobranov <pavel.d@alcyonrisk.com>
2017-09-15 12:45:11 +0300

    Added: webpack configuration, tweeter bootstrap

--- client/src/app/app.module.js
+++ client/src/app/app.module.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { appComponent } from './app.component';
 
 export const app = angular
   .module('app', [uiRouter])
